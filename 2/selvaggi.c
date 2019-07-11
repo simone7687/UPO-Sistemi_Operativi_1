da7d89ca9abdcc189929a7fef59b4d25b52ea0d2
Simone Massaro <simonemassaro7@gmail.com>
2019-07-11 13:58:49 +0200

    Il numero di selvaggi, di porzioni e di giri dovranno essere richiesti come argomenti #9

--- 2/selvaggi.c
+++ 2/selvaggi.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <semaphore.h>  // per utilizare le system call POSIX per la gestione di semafori (sem_init, sem_wait, sem_post)
 #include <pthread.h>    // per la gestione dei thread
+#include <stdlib.h>
 
 int N;  /* numero selvaggi */
 int M;  /* numero porzioni */
@@ -32,7 +33,18 @@ Selvaggio()
     }
 }
 
-int main()
+int main(int argc, char *argv[])
 {
+    // Il numero di selvaggi, di porzioni e di giri dovranno essere richiesti come argomenti #9
+    if (argc != 4)
+    {
+        fprintf(stderr, "%s <numero_selvaggi> <numero_porzioni> <numero_giri>'\n", argv[0]);
+        exit(1);
+    }
+    N = atoi(argv[1]);
+    M = atoi(argv[2]);
+    NGIRI = atoi(argv[3]);
+    printf("numero selvaggi = %d  \nnumero porzioni = %d  \nnumero giri     = %d\n", N, M, NGIRI);
+    
     pentola = M;
 }
